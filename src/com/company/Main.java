aba81503df031e89b5241ca21445669dba39edaa
Khadar Ahmed Yusuf <Khadar_yo@hotmail.com>
2021-08-30 15:55:30 +0200

    added a lowestNr method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] a = {2,4,6,12,32,21,0,14,32,1,8};
+        int[] a = {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
         System.out.println(highestNr(a));
+        System.out.println(lowestNr(a));
     }
 
 
@@ -14,6 +15,22 @@ public class Main {
         for(int i = 1; i < arr.length; i++) {
             if(arr[i] > arr[m]) m = i;
         }
-        return arr[m];
+        return m;
+    }
+
+    public static int lowestNr(int[] arr) {
+        //Initialiser med første element
+        int m = 0;
+
+        //Loop over alle resterende elemeneter og sjekk om
+        // vi finner et mindre tall.
+        for(int i = 1; i < arr.length; i++) {
+
+            //Sjekker om verdien er mindre enn det vi har sett tidligere.
+            if(arr[i] < arr[m]) m = i;
+        }
+
+        //Returner index til minste verdi.
+        return m;
     }
 }
