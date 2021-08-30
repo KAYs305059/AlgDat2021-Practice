8a16278c0dc65039623990e7146f839f0ae2a4c1
Khadar Ahmed Yusuf <Khadar_yo@hotmail.com>
2021-08-30 12:36:15 +0200

    added a highestNr method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] a = {2,4,6,12,32,21,0,14,32,1,8};
+        System.out.println(highestNr(a));
+    }
+
+
+    public static int highestNr(int[] arr) {
+        int m = 0;
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > arr[m]) m = i;
+        }
+        return arr[m];
     }
 }
